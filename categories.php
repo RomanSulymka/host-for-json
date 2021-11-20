c4ec46741663c4d5bb74d87db698e463334350c2
Roman Sulymka <44112418+RomanSulymka@users.noreply.github.com>
2021-11-20 20:13:14 +0200

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,40 @@
+{
+    categories: [
+      {
+        idCategory: "1",
+        strCategory: "Company",
+        strCategoryThumb: "https://www.bestseocompanies.com/wp-content/uploads/2018/11/industry-1.png",
+        strCategoryDescription: "Here is the topic with basic information about the company in which you work.[1]"
+      },
+      {
+        idCategory: "2",
+        strCategory: "Information",
+        strCategoryThumb: "https://icon-library.com/images/basic-information-icon/basic-information-icon-14.jpg",
+        strCategoryDescription: "section with answers to frequently asked questions."
+      },
+      {
+        idCategory: "3",
+        strCategory: "Progress",
+        strCategoryThumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyalLx_79QPRGAA9a8a5oNsBoh-ZYnB1kOQw&usqp=CAU",
+        strCategoryDescription: "In this section you can track your current progress."
+      },
+      {
+        idCategory: "4",
+        strCategory: "Tasks",
+        strCategoryThumb: "https://www.17games.info/android/programmi/ofisnie/google-tasks-any-task-any-goal-get-things-done/google-tasks-any-task-any-goal-get-things-done.png",
+        strCategoryDescription: "In this section are your tasks."
+      },
+      {
+        idCategory: "5",
+        strCategory: "Tutorials",
+        strCategoryThumb: "https://sketchupschool.imgix.net/assets/sketchup-definitive-guide/icons/chapters/sm/ch3-how-to-learn-sketchup-ee9c1b962c9d2f05cd2af269fed29d5e.png?auto=compress%2Cformat%2Cenhance&ixlib=imgixjs-3.3.2",
+        strCategoryDescription: "In this section, you can watch useful videos and improve your performance"
+      },
+      {
+        idCategory: "6",
+        strCategory: "Voting",
+        strCategoryThumb: "https://www.ischool.berkeley.edu/sites/default/files/styles/fullscreen/public/sproject_teaser_image/voted.png?itok=b--NMxns",
+        strCategoryDescription: "Here you can cast your vote when the company conducts the poll."
+      }
+    ]
+  }
